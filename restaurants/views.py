54052c170272fc0341834c4ed5f0a05b46e3360d
Farid Rifaie <frifaie@yahoo.com>
2017-08-28 12:43:00 +0700

    D62 100DOC

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,7 +1,8 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello")
-    #return render(request, 'template.html', {})
+    num = random.randint(0, 100000)
+    return render(request, 'base.html', {"html_var": "context var", "num": num})
