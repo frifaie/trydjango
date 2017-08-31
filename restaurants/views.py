c8e1b14f3a33a6a12390e44cbbf06cac9c6be1b0
Farid Rifaie <frifaie@yahoo.com>
2017-08-31 13:20:59 +0700

    D65 100DOC

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,23 +2,15 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
-def home(request):
-    num = random.randint(0, 100000)
-    a_list = [num, random.randint(0, 100000), random.randint(0, 100000)]
-    context = {"html_var": True, "num": num, "a_list": a_list}
-    return render(request, 'home.html', context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-def about(request):
-    context = {}
-    return render(request, 'about.html', context)
-
-def contact(request):
-    context = {}
-    return render(request, 'contact.html', context)
-
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, 'contact.html', context)
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = random.randint(0, 100000)
+        a_list = [num, random.randint(0, 100000), random.randint(0, 100000)]
+        context = {"html_var": True, "num": num, "a_list": a_list}
+        return context
