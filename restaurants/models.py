bec4cfb6451af26046ec7213f1ff92bfb81c33e5
Farid Rifaie <frifaie@yahoo.com>
2017-09-01 23:08:10 +0700

    D66 100DOC

--- restaurants/models.py
+++ restaurants/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class RestaurantLocation(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120, null=True, blank=True)
+    category = models.CharField(max_length=120, null=True, blank=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,16 +1,11 @@
-import random
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
+from .models import RestaurantLocation
 # Create your views here.
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        num = random.randint(0, 100000)
-        a_list = [num, random.randint(0, 100000), random.randint(0, 100000)]
-        context = {"html_var": True, "num": num, "a_list": a_list}
-        return context
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {"object_list": queryset}
+    return render(request, template_name, context)
