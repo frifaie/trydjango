1c10cde82f3f5d5a6a7c7ea5dae4704019b05df5
Farid Rifaie <frifaie@yahoo.com>
2017-09-04 14:39:41 +0700

    D69 100DOC

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import RestaurantLocation
 # Create your views here.
@@ -9,3 +10,24 @@ def restaurant_listview(request):
     queryset = RestaurantLocation.objects.all()
     context = {"object_list": queryset}
     return render(request, template_name, context)
+
+class RestaurantListView(ListView):
+    """docstring for RestaurantListView."""
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+            Q(category__iexact = slug) |
+            Q(category__icontains = slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id)
+        return obj
